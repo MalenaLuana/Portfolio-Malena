5231ae58c5457f7f3a31629bd5b783b41f884cbb
MalenaLuana <malenaluana98@gmail.com>
2023-01-09 19:44:40 -0300

    about + responsive form (9/1)

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState,useRef  } from "react";
-
+import {motion}from 'framer-motion'
 import style from '../../css/About.module.css'
 
 
@@ -10,9 +10,11 @@ export default function About (){
 
  const [hidden,sethidden] = useState('0')
 
-
+ const [text, setText] = useState('0')
 
  const divRef = useRef()
+
+ 
  
 useEffect(()=>{
   
@@ -21,8 +23,10 @@ useEffect(()=>{
       const div = divRef.current
       const {y}=div.getBoundingClientRect()
       
-     let ishidden = y<=700? '100%':'0'
-     if(hidden=== '0') sethidden(ishidden)
+      let ishidden = y<=700? '100%':'0'
+      let showText = y<=600? '25pt':'0'
+      if(hidden=== '0') sethidden(ishidden)
+      setText(showText)
     }
 
     window.addEventListener('scroll',handleScroll)
@@ -36,20 +40,38 @@ useEffect(()=>{
         <div className={style.container} >
         
         <div className={style.ancla} id='about'>
-        <div  ref={divRef} style={{width:hidden,transition:'all ease 0.5s'}}>
+        <div className={style.red} style={{width:hidden,transition:'all ease 0.9s'}}></div>
+        <div  ref={divRef} >
         <div className={style.content}>
  
           
           <div className={style.card} tabIndex={0}>
             <div className={style.box}>
-            <div className={style.front}>QUIEN SOY?</div>
-            <div className={style.back}>info</div>
+            <div className={style.front} style={{fontSize:text,transition:'all ease 1s'}}>QUIÉN SOY?</div>
+            <div className={style.back}>
+              <div className={style.backText}>Desarrolladora web full stack y artista visual.<br/>Tengo 24 años, soy de Argentina, Bs As y desde muy chica me dedico a crear todo tipo de cosas. <br /> Ya sean pinturas, esculturas, tatuajes o, en este caso, PÁGINAS WEBS  </div>
+            </div>
             </div>
           </div>
 
 
-          <div className={style.card2}><div>CUALES SON <br/>MIS OBJETIVOS?</div></div>
-          <div className={style.card3}><div>QUE HAGO PARA <br/>NO ABURRIRME?</div></div>
+          <div className={style.card} tabIndex={0}>
+            <div className={style.box}>
+            <div className={style.front} style={{fontSize:text,transition:'all ease 1s'}}>CUÁLES SON <br/>MIS OBJETIVOS?</div>
+            <div className={style.back}>
+              <div className={style.backText}> Este 2023 mi principal objetivo es conseguir mi primer experiencia laboral en el rubro IT. <br /> Encontrar el lugar que me permita desarrollarme profesionalmente y crecer a nivel personal. Con esto mejorar mi día a día, aprender cosas nuevas y darme un mejor estilo de vida.</div>
+            </div>
+            </div>
+          </div>
+
+          <div className={style.card}tabIndex={0}>
+          <div className={style.box}>
+            <div className={style.front} style={{fontSize:text,transition:'all ease 1s'}}>QUÉ HAGO PARA <br/>NO ABURRIRME?</div>
+            <div className={style.back}>
+              <div className={style.backText}>◽Jugar video juegos 🎮<br /> ◽Cantar y componer canciones 🎹 <br />◽Ir a comer a mis lugares favoritos 💜</div>
+            </div>
+          </div>
+          </div>
         </div>
         </div>
         </div>
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,13 +3,23 @@ import style  from '../css/Banner.module.css'
 import image from '../css/images/bannerGirl.png'
 import left from '../css/images/codeLeft.png'
 import right from '../css/images/codeRight.png'
-
+import {motion}from 'framer-motion'
 
 export default function Banner(){
     return (
         <div className={style.banner} id='banner'>
-            <div className={style.cont}>
-            <div className={style.text} >
+            <motion.div className={style.cont}
+              initial={{x:'-110%'}}
+              animate={{x:'0%'}}
+              transition={{
+                  duration:1.5,
+                 
+                  delay:0.2,
+                  type:'spring'
+              }}>
+            <div className={style.text} 
+           
+            >
             <h1 className={style.one}>BIENVENIDXS</h1>
             <div className={style.middle}>
                 <img src={left} alt="" />
@@ -22,7 +32,7 @@ export default function Banner(){
             <div>
              <img src={image} className={style.image}alt="" />
             </div>  
-           </div>
+           </motion.div>
         </div>
     )
 }
